eba30931b5d5ee9d77bdcb4f1eec66f8e5242915
Chidambararajab <chidambararaja97@gmail.com>
2021-07-28 09:20:43 +0530

    Commiting Basic Setup

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter App',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter App'),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+                width: double.infinity,
+                child: Card(
+                  color: Colors.blue,
+                  child: Text('Simple Card'),
+                )),
+            Container(
+                color: Colors.blue,
+                child: Card(
+                  child: Text('List of TX'),
+                ))
+          ],
+        ));
+  }
+}
